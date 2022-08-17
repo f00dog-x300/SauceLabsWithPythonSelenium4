b285d6b255f4b1409b250bb537941b41b416bf64
klao <f00dog.x300@gmail.com>
2022-08-17 15:55:49 +0800

    dynamic loading pages in progress

--- tests/test_dynamic_loading.py
+++ tests/test_dynamic_loading.py
@@ -4,5 +4,5 @@ from pages.dynamic_loading_pages import DynamicLoadingPage
 def test_finish_loading_page(dynamic_loading: DynamicLoadingPage):
     """Testing with valid credentials. Should show success message."""
     dynamic_loading.click_start_button()
-    assert dynamic_loading.is_loading_bar_present()
+    # assert dynamic_loading.is_loading_bar_present()
     assert dynamic_loading.is_hello_world_text_present()
